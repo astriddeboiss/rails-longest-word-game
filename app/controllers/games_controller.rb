0eac5a853bff560e8f6f99330c4d88a8f4eb4f4f
Astrid de Boissieu <astrid.de-boissieu@hec.edu>
2018-03-05 14:47:04 +0100

    exo2

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,39 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @attempt = params[:attempt]
+   if @attempt.chars.all? { |letter| @attemps.count(letter) <= @letters.count(letter) }
+     if english_word?(attempt)
+       "well done"
+     else
+       "not an english word"
+     end
+   else
+      "not in the grid"
+   end
+  end
+
+
+def run_game(attempt)
+
+  score_and_message = score_and_message(attempt, grid, result[:time])
+  result[:score] = score_and_message.first
+  result[:message] = score_and_message.last
+
+  result
+end
+
+
+
+def english_word?(word)
+  response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+  json = JSON.parse(response.read)
+  return json['found']
+end
